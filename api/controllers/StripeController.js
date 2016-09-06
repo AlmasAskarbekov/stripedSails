cbb8098e37f2d89a50c6acc788628db1419872bb
Jasper <jperforest@gmail.com>
2016-09-06 13:42:17 -0700

    Added ability to add cards to customers, charge customers individually or in mass

--- api/controllers/StripeController.js
+++ api/controllers/StripeController.js
@@ -4,8 +4,9 @@
  * @description :: Server-side logic for managing stripes
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
- var stripeConnect = require('stripe')('sk_test_9j9IxZ0rLxWeMZqtAlgKf2HR');
 
+var stripeConnect = require('stripe')('sk_test_9j9IxZ0rLxWeMZqtAlgKf2HR');
+var async = require('async');
 
 module.exports = {
 
@@ -31,7 +32,7 @@ module.exports = {
      * Adds a charge amount to a specific customer.
      *
      * @param id, customer id
-     * @param amount, a number amoutn greater than zero to add to charges.
+     * @param amount, the number of cents greater than zero to add to charges.
      */
     addCharge : function(req, res){
         if(req.body && req.body.id && req.body.amount){
@@ -46,7 +47,7 @@ module.exports = {
                     if(!stripeCustomer.amount){
                         stripeCustomer.amount = [];
                     }
-                    stripeCustomer.amount.push(req.body.amount);
+                    stripeCustomer.amount.push(Math.round(req.body.amount));
                     stripeCustomer.save(function(err){
                         if(err){
                             res.serverError({
@@ -79,14 +80,166 @@ module.exports = {
      * @param percent, (optional) percent of total to charge.
      */
     chargeSpecific: function(req, res){
-
+        if(req.body && req.body.id){
+            Stripe.findOne({id: req.body.id}, function(err, stripe){
+                if(err || !stripe){
+                    res.serverError({
+                        error: err || 'No customer found with that Id.',
+                        requestBody: req.body
+                    });
+                }
+                else{
+                    stripeConnect.customers.retrieve(stripe.stripeId, function(err, stripeCustomer){
+                        if(err || !stripeCustomer){
+                            res.serverError({
+                                error: err || 'No customer found with that Id.',
+                                requestBody: req.body
+                            });
+                        }
+                        else if(!stripe.amount.length){
+                            res.serverError({
+                                error: 'Customer has not amounts to charge',
+                                requestBody: req.body
+                            })
+                        }
+                        else{
+                            Stripe.charge(stripe, stripeCustomer, req.body.percent, function(err, result){
+                                if(err){
+                                    res.serverError({
+                                        error : err,
+                                        requestBody:req.body
+                                    });
+                                }
+                                else{
+                                    res.send(result);
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        }
+        else{
+            res.serverError({
+                error: "Missing Parameters / invalid Parameters",
+                requestBody: req.body,
+                requiredBody: {
+                    id: 'user id',
+                    percent: 'decmial of % charge (optional, defaults to 1)'
+                }
+            });
+        }
     },
 
     /**
      * Charges all customer for full amounts.
-     *
      */
     chargeAll : function(req, res){
+        Stripe.find({}, function(err, stripes){
+            if(err){
+                res.serverError({
+                    error: err,
+                    requestBody: req.body
+                });
+            }
+            else{
+                var results = [];
+                var errors = [];
+                async.each(stripes, function(stripe, nxt){
+                    stripeConnect.customers.retrieve(stripe.stripeId, function(err, stripeCustomer){
+                        if(err || !stripeCustomer){
+                            errors.push({
+                                error: err || 'No customer found with that Id.',
+                                requestBody: req.body
+                            });
+                            nxt();
+                        }
+                        else if(!stripe.amount.length){
+                            errors.push({
+                                error: 'Customer has not amounts to charge',
+                                requestBody: req.body
+                            })
+                            nxt();
+                        }
+                        else{
+                            Stripe.charge(stripe, stripeCustomer, req.body.percent, function(err, result){
+                                if(err){
+                                    errors.push({
+                                        error : err,
+                                        requestBody:req.body
+                                    });
+                                    nxt();
+                                }
+                                else{
+                                    results.push(result);
+                                    nxt();
+                                }
+                            });
+                        }
+                    });
+                }, function(err){
+                    if(err){
+                        res.serverError({
+                            error: err,
+                            requestBody:req.body
+                        })
+                    }
+                    else{
+                        res.send({
+                            successful:results,
+                            failed: errors
+                        });
+                    }
+                })
+            }
+        });
+    },
 
+
+    /**
+     * Adds the given source to a customer in stripe.
+     * @param id, user id,
+     * @param number, Credit card number
+     * @param month, Credit card expiration month
+     * @param year, Credit card expiration year
+     * @param cvc, Credit card cvc number
+     */
+    addCard: function(req, res){
+        if(req.body && req.body.id && req.body.number && req.body.month && req.body.year && req.body.cvc){
+            Stripe.findOne({id: req.body.id}, function(err, stripe){
+                if(err || !stripe){
+                    res.serverError({
+                        error: err || 'No customer matching that id',
+                        requestBody: req.body
+                    });
+                }
+                else{
+                    Stripe.addCard(stripe, req.body, function(err, result){
+                        if(err){
+                            res.serverError({
+                                error: err || 'No customer matching that id',
+                                requestBody: req.body
+                            });
+                        }
+                        else{
+                            res.send(result);
+                        }
+                    });
+                }
+            });
+        }
+        else{
+            res.serverError({
+                error: "Missing Parameters / invalid Parameters",
+                requestBody: req.body,
+                requiredBody: {
+                    id     : 'user id',
+                    number : 'CC number',
+                    month : 'CC exp month',
+                    year   : 'CC exp year',
+                    cvc    : 'CC cvc number'
+                }
+            });
+        }
     }
 };
--- api/models/Stripe.js
+++ api/models/Stripe.js
@@ -4,7 +4,7 @@
 * @description :: Stripe User clone with addition information to store charge amounts.
 * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
 */
-var stripe = require('stripe')('sk_test_9j9IxZ0rLxWeMZqtAlgKf2HR');
+var stripeConnect = require('stripe')('sk_test_9j9IxZ0rLxWeMZqtAlgKf2HR');
 module.exports = {
 
     attributes: {
@@ -24,9 +24,9 @@ module.exports = {
             type:'array'
         },
 
-        stripeId:{
+        // Stripe customer Id
+        stripeId: {
             type: 'string'
-            unqiue:true
         }
     },
 
@@ -34,7 +34,7 @@ module.exports = {
      * After the customer is created in our system we will create a stripe customer.s
      */
     afterCreate: function(newRec,cb){
-        stripe.customers.create({
+        stripeConnect.customers.create({
             email:newRec.email,
             metadata: newRec
         }, function(err, customer){
@@ -56,7 +56,7 @@ module.exports = {
      * Validates updates data past normal validation.
      */
     beforeUpdate: function(data, cb){
-        if(invalidAmount(data.amount)){
+        if(data.amount !== undefined && invalidAmount(data.amount)){
             cb('Invalid amount was passed to updater: ' + data.amount);
         }
         else{
@@ -70,12 +70,12 @@ module.exports = {
     beforeDestroy: function(destRec, cb){
         Stripe.findOne(destRec, function(err, stripeCustomer){
             if(stripeCustomer){
-                stripe.customers.retrieve(stripeCustomer.stripeId, function(err, customer){
+                stripeConnect.customers.retrieve(stripeCustomer.stripeId, function(err, customer){
                     if(err || !customer){
                         cb(err || 'no customer found.');
                     }
                     else{
-                        stripe.customers.del(customer.id, function (err, suc) {
+                        stripeConnect.customers.del(customer.id, function (err, suc) {
                             cb(err);
                         });
                     }
@@ -85,11 +85,88 @@ module.exports = {
                 cb(err);
             }
         });
+    },
+
+    /**
+     * Charges a given customer's amounts by the percent passed in.
+     * @param customer, database customer,
+     * @param stripeCustomer, stripe customer,
+     * @param percent, percent to charge customer (defaults to 1)
+     * @param cb, callback function(err, results)
+     */
+    charge: function(customer, stripeCustomer, percent , cb){
+
+        // Defaults percent to 1, (node.js es6 default parameters work around)
+        percent = percent || 1;
+
+        if(stripeCustomer.sources.data.length){
+            var total = 0;
+            customer.amount.forEach(function(amt){
+                total+= amt;
+            });
+            const chargeAmount =  Math.round(total * percent);
+            const remainder = total - chargeAmount;
+            stripeConnect.charges.create({
+                amount: chargeAmount,
+                currency: 'usd',
+                customer: stripeCustomer.id
+            }, function(err, result){
+                if(err){
+                    cb(err, result);
+                }
+                else{
+                    if(!customer.charges){
+                        customer.charges = [];
+                    }
+                    if(!!remainder){
+                        customer.amount = [remainder];
+                    }
+                    else{
+                        customer.amount = [];
+                    }
+                    customer.charges.push(result);
+                    customer.save(function(err){
+                        cb(err, result);
+                    });
+                }
+            });
+        }
+        else{
+            cb('Customer has not stored credit cards: ' + customer.id, stripeCustomer);
+        }
+    },
+
+    /**
+     * Adds a card to a stripe customer.
+     * @param customer, customer info
+     * @param cardInfo, object with card details,
+     * @param cb, callback function(err, result)
+     */
+    addCard : function(customer, cardInfo, cb){
+        stripeConnect.tokens.create({
+            card: {
+                "number" :cardInfo.number,
+                "exp_month": cardInfo.month,
+                "exp_year" : cardInfo.year,
+                "cvc" : cardInfo.cvc
+            }
+        }, function(err, token){
+            if(err){
+                cb(err, token);
+            }
+            else{
+                stripeConnect.customers.update(customer.stripeId, {
+                    source: token.id
+                }, function(err, result){
+                    cb(err, result);
+                });
+            }
+        });
     }
 };
 /**
  * Helper function to valid number amount
  */
 function invalidAmount(amount){
-    return (amount === undefined) || (typeof amount != 'number' && amount <= 0);
+    return !(!!amount && !amount.length) && ( (amount === undefined) || (typeof amount != 'number' && amount <= 0) );
 }
